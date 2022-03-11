83116e9f514589b3d21aa02f2661c2dc8f3975e8
황재성님/AI Analytics팀 <postechiminuru@gmail.com>
2022-03-11 17:46:16 +0900

    Push simulation code for Yahoo data

---
+++ yahoo/dataset.py
@@ -0,0 +1,174 @@
+"""
+Line format for yahoo events:
+1241160900 109513 0 |user 2:0.000012 3:0.000000 4:0.000006 5:0.000023 6:0.999958 1:1.000000 |109498 2:0.306008 3:0.000450 4:0.077048 5:0.230439 6:0.386055 1:1.000000 |109509 2:0.306008 3:0.000450 4:0.077048 5:0.230439 6:0.386055 1:1.000000 [[...more article features omitted...]] |109453 2:0.421669 3:0.000011 4:0.010902 5:0.309585 6:0.257833 1:1.000000
+
+Some log files contain rows with erroneous data.
+
+After the first 10 columns are the articles and their features.
+Each article has 7 columns (articleid + 6 features)
+Therefore number_of_columns-10 % 7 = 0
+"""
+
+import numpy as np
+import fileinput
+
+
+def make_data_from_yahoo_events(filenames):
+    """
+    Reads a stream of events from the list of given files.
+    
+    Parameters
+    ----------
+    filenames : list
+        List of filenames
+    
+    Stores
+    -------    
+    articles : [article_ids]
+    features : [[article_1_features] .. [article_n_features]]
+    events : [
+                 0 : displayed_article_index (relative to the pool),
+                 1 : user_click,
+                 2 : [user_features],
+                 3 : [pool_indexes]
+             ]
+    """
+
+    global X, events, displays, rewards, articles, features, n_arms, n_events
+    X = []
+    events = []
+    displays = []
+    rewards = []
+    articles = []
+    features = []
+    
+
+    skipped = 0
+
+    with fileinput.input(files=filenames) as f:
+        for line in f:
+            cols = line.split()
+            if (len(cols) - 10) % 7 != 0:
+                skipped += 1
+            else:
+                pool_idx = []
+                pool_ids = []
+                for i in range(10, len(cols) - 6, 7):
+                    id = cols[i][1:]
+                    if id not in articles:
+                        articles.append(id)
+                        features.append([float(x[2:]) for x in cols[i + 1: i + 7]])
+                    pool_idx.append(articles.index(id))
+                    pool_ids.append(id)
+                X.append(np.expand_dims(np.array([float(x[2:]) for x in cols[4:10]]),0))
+                displays.append(articles.index(cols[1]))
+                rewards.append(int(cols[2]))
+                events.append(
+                    [
+                        pool_ids.index(cols[1]),
+                        pool_idx
+                    ]
+                )
+                
+    features = np.array(features)
+    X = np.concatenate(X)
+    n_arms = len(articles)
+    n_events = len(X)
+    print(n_events, "events with", n_arms, "articles")
+    if skipped != 0:
+        print("Skipped events:", skipped)
+
+
+
+def get_yahoo_events(filenames):
+    """
+    Reads a stream of events from the list of given files.
+    
+    Parameters
+    ----------
+    filenames : list
+        List of filenames
+    
+    Stores
+    -------    
+    articles : [article_ids]
+    features : [[article_1_features] .. [article_n_features]]
+    events : [
+                 0 : displayed_article_index (relative to the pool),
+                 1 : user_click,
+                 2 : [user_features],
+                 3 : [pool_indexes]
+             ]
+    """
+
+    global articles, features, events, n_arms, n_events
+    articles = []
+    features = []
+    events = []
+
+    skipped = 0
+
+    with fileinput.input(files=filenames) as f:
+        for line in f:
+            cols = line.split()
+            if (len(cols) - 10) % 7 != 0:
+                skipped += 1
+            else:
+                pool_idx = []
+                pool_ids = []
+                for i in range(10, len(cols) - 6, 7):
+                    id = cols[i][1:]
+                    if id not in articles:
+                        articles.append(id)
+                        features.append([float(x[2:]) for x in cols[i + 1: i + 7]])
+                    pool_idx.append(articles.index(id))
+                    pool_ids.append(id)
+
+                events.append(
+                    [
+                        pool_ids.index(cols[1]),
+                        int(cols[2]),
+                        [float(x[2:]) for x in cols[4:10]],
+                        pool_idx,
+                    ]
+                )
+    features = np.array(features)
+    n_arms = len(articles)
+    n_events = len(events)
+    print(n_events, "events with", n_arms, "articles")
+    if skipped != 0:
+        print("Skipped events:", skipped)
+
+
+def max_articles(n_articles):
+    """
+    Reduces the number of articles to the threshold provided.
+    Therefore the number of events will also be reduced.
+
+    Parameters
+    ----------
+    n_articles : number
+        number of max articles after reduction
+    """
+
+    global articles, features, events, n_arms, n_events
+    assert n_articles < n_arms
+
+    n_arms = n_articles
+    articles = articles[:n_articles]
+    features = features[:n_articles]
+
+    for i in reversed(range(len(events))):
+        displayed_pool_idx = events[i][0]  # index relative to the pool
+        displayed_article_idx = events[i][3][
+            displayed_pool_idx
+        ]  # index relative to the articles
+
+        if displayed_article_idx < n_arms:
+            events[i][0] = displayed_article_idx
+            events[i][3] = np.arange(0, n_arms)  # pool = all available articles
+        else:
+            del events[i]
+
+    n_events = len(events)
+    print("Number of events:", n_events)
